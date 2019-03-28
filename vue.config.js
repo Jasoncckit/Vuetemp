ac7768966c6addac44e9ddadbc28aa0b9b9a7093
Jason Cheng <Jasoncckit@gmail.com>
2019-03-28 14:48:54 +1100

    Updated deprecated syntax

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  baseUrl: './',
+  publicPath: './',
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
   productionSourceMap: false,
+  parallel: undefined,
+  css: undefined,
+  lintOnSave: undefined,
+
   pwa: {
     name: 'Vuetemp',
     themeColor: '#FAFAFA'
